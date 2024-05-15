6186e220996c975adfdd2ec5ba5c53fbd2e69f79
Abdulrahman <emadabdo258@gmail.com>
2024-05-15 22:38:19 +0300

    "Page Elements package"

--- src/main/java/Pages/UserPortal/CheckList.java
+++ src/main/java/Pages/UserPortal/CheckList.java
@@ -6,7 +6,6 @@ import CoreElements.TextBox;
 import Pages.PagesElements.CheckListElements;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 public class CheckList {
 

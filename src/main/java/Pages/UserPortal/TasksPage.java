08d933c86c0e5062ece4caf6ef7a075a1c9bbe71
Abdulrahman <emadabdo258@gmail.com>
2024-05-15 22:27:09 +0300

    "Page Elements package"

--- src/main/java/Pages/UserPortal/TasksPage.java
+++ src/main/java/Pages/UserPortal/TasksPage.java
@@ -3,7 +3,6 @@ package Pages.UserPortal;
 import CoreElements.Element;
 import CoreElements.Label;
 import Pages.PagesElements.TasksElements;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 public class TasksPage {
     WebDriver driver;
